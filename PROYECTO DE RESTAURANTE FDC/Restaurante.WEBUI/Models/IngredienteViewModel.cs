9022b6b2ee5f102b811aed98d580992f75c673f7
Phynomo <phynomo09099@gmail.com>
2023-05-03 20:17:25 -0600

    Ya

--- PROYECTO DE RESTAURANTE FDC/Restaurante.WEBUI/Models/IngredienteViewModel.cs
+++ PROYECTO DE RESTAURANTE FDC/Restaurante.WEBUI/Models/IngredienteViewModel.cs
@@ -7,5 +7,17 @@ namespace Restaurante.WEBUI.Models
 {
     public class IngredienteViewModel
     {
+        public int ingr_Id { get; set; }
+        public string ingr_Nombre { get; set; }
+        public decimal ingr_PrecioX100gr { get; set; }
+        public int prov_Id { get; set; }
+        public string prov_NombreEmpresa { get; set; }
+        public DateTime ingr_FechaCreacion { get; set; }
+        public int ingr_UsuCreacion { get; set; }
+        public string user_NombreUsuCreacion { get; set; }
+        public DateTime? ingr_FechaModificacion { get; set; }
+        public int? ingr_UsuModificacion { get; set; }
+        public string user_NombreUsuModificacion { get; set; }
+        public bool ingr_Estado { get; set; }
     }
 }
--- PROYECTO DE RESTAURANTE FDC/Restaurante.WEBUI/Models/SucuarsalViewModel.cs
+++ PROYECTO DE RESTAURANTE FDC/Restaurante.WEBUI/Models/SucuarsalViewModel.cs
@@ -7,5 +7,21 @@ namespace Restaurante.WEBUI.Models
 {
     public class SucuarsalViewModel
     {
+        public int sucu_Id { get; set; }
+        public string sucu_Nombre { get; set; }
+        public int muni_Id { get; set; }
+        public string muni_Nombre { get; set; }
+        public string muni_Codigo { get; set; }
+        public int depa_Id { get; set; }
+        public string depa_Nombre { get; set; }
+        public string depa_Codigo { get; set; }
+        public string sucu_Direccion { get; set; }
+        public int sucu_UsuCreacion { get; set; }
+        public string user_NombreUsuCreacion { get; set; }
+        public DateTime? sucu_FechaCreacion { get; set; }
+        public int? sucu_UsuModificacion { get; set; }
+        public string user_NombreUsuModificacion { get; set; }
+        public DateTime? sucu_FechaModificacion { get; set; }
+        public bool? sucu_Estado { get; set; }
     }
 }
