06392dbca1056932599d22ab3a0d742d3f8a709f
Phynomo <123413407+Phynomo@users.noreply.github.com>
2023-05-08 10:36:15 -0600

    No sé

--- PLANTILLAS/costic/src/components/sections/pruebas/Content.js
+++ PLANTILLAS/costic/src/components/sections/pruebas/Content.js
@@ -1,32 +1,14 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
 import SelectMultiple from './select';
-
-class Content extends Component {
-    componentDidMount() {
-        //initialize datatable
-        function animateRoundedProgress(rpb) {
-            if (!$(rpb).hasClass('animated')) {
-                $(rpb).css('stroke-dashoffset', 358.141563 - (358.141563 / 100) * $(rpb).attr('aria-valuenow'));
-                $(rpb).addClass('animated');
-            }
-        }
-        function initRoundedProgress() {
-            var roundedProgress = $('.progress-cicle');
-            roundedProgress.each(function () {
-                animateRoundedProgress(this);
-            });
-        }
-        initRoundedProgress()
-    }
-    render() {
-        return (
-            <div className="ms-content-wrapper">
-                <SelectMultiple></SelectMultiple>
-            </div>
-
-        );
-    }
-}
-
-export default Content;
+import generatePDF from './pdf';
+import { PDFViewer } from '@react-pdf/renderer';
+const PreviewPDF = () => (
+    <div style={{ width: '100%', height: '100vh' }}>
+      <PDFViewer style={{ width: '100%', height: '100%' }}>
+        <generatePDF />
+      </PDFViewer>
+    </div>
+  );
+  
+  export default PreviewPDF;
